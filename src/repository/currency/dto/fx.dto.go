903a72d37f9cbb84ab67bba5e56d7c38a056409b
井原 樹/Miki Ihara <kimity0824@icloud.com>
2023-12-06 02:14:30 +0900

    currencyRepository実装

---
+++ src/repository/currency/dto/fx.dto.go
@@ -0,0 +1,16 @@
+package dto
+
+// CurrencyPair は通貨ペアの情報を表します。
+type CurrencyPair struct {
+    High             string  `json:"high"`
+    Open             string  `json:"open"`
+    Bid              string `json:"bid"`
+    CurrencyPairCode string  `json:"currencyPairCode"`
+    Ask              string  `json:"ask"`
+    Low              string  `json:"low"`
+}
+
+// Fx は通貨のレート情報を表します。
+type Fx struct {
+    Quotes []CurrencyPair `json:"quotes"`
+}
