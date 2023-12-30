b7c1a45abd091335cdfce366db6b57882fcd20fd
井原 樹/Miki Ihara <kimity0824@icloud.com>
2023-12-30 15:21:57 +0900

    e2e実装

---
+++ test/graphql/crypto/crypto.e2e_test.go
@@ -0,0 +1,209 @@
+package crypto
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"my-us-stock-backend/app/database/model"
+	repoMarketPrice "my-us-stock-backend/app/repository/market-price/crypto"
+	"my-us-stock-backend/test"
+	"my-us-stock-backend/test/graphql"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// MockHTTPTransport は http.RoundTripper のインターフェースを満たすモック実装です。
+type MockHTTPTransport struct {
+    RoundTripFunc func(req *http.Request) (*http.Response, error)
+}
+
+// RoundTrip は http.RoundTripper の RoundTrip メソッドを模倣します。
+func (m *MockHTTPTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+    return m.RoundTripFunc(req)
+}
+
+func TestCryptosE2E(t *testing.T) {
+	db := test.SetupTestDB(t)
+
+	// モックの HTTP レスポンスを設定
+	mockCryptoPrice := `{
+		"success": 1,
+		"data": {
+			"sell": "5958001",
+			"buy": "5958000",
+			"open": "6052000",
+			"high": "6127930",
+			"low": "5900000",
+			"last": "5956517",
+			"vol": "335.3697",
+			"timestamp": 1703916551294
+		}
+	}`
+
+	// モックのHTTPクライアント設定
+	mockTransport := &MockHTTPTransport{
+		RoundTripFunc: func(req *http.Request) (*http.Response, error) {
+			responseBody := mockCryptoPrice
+			r := io.NopCloser(bytes.NewReader([]byte(responseBody)))
+			return &http.Response{
+				StatusCode: http.StatusOK,
+				Body:       r,
+			}, nil
+		},
+	}
+
+	mockHTTPClient := &http.Client{Transport: mockTransport}
+	mockMarketPriceRepo := repoMarketPrice.NewCryptoRepository(mockHTTPClient)
+	// オプションを使用してGraphQLサーバーをセットアップ
+	opts := &graphql.SetupOptions{
+		MockHTTPClient: mockHTTPClient,
+		// MarketCryptoRepoにモックリポジトリを指定
+		MarketCryptoRepo: mockMarketPriceRepo,
+	}
+    router := graphql.SetupGraphQLServer(db, opts)
+
+    // テスト用HTTPサーバーのセットアップ
+    ts := httptest.NewServer(router)
+    defer ts.Close()
+
+    // テスト用のユーザーを作成
+    db.Create(&model.Crypto{Code: "btc", UserId: 1, Quantity: 0.05, GetPrice: 5047113.0})
+
+	// ダミーのアクセストークンを生成
+	token, err := graphql.GenerateTestAccessTokenForUserId(t, 1)
+	if err != nil {
+		t.Fatalf("Failed to generate test access token: %v", err)
+	}
+
+	// GraphQLリクエストの実行
+	query := `query {
+		cryptos{ id code getPrice quantity currentPrice }
+	  }`
+	  w := graphql.ExecuteGraphQLRequestWithToken(ts.URL, query, token)
+
+	// レスポンスボディの解析
+    var response struct {
+        Data struct {
+            Cryptos []struct {
+                ID string `json:"id"`
+				Code string `json:"code"`
+				GetPrice float64 `json:"getPrice"`
+				Quantity float64 `json:"quantity"`
+                CurrentPrice float64 `json:"currentPrice"`
+            } `json:"cryptos"`
+        } `json:"data"`
+    }
+
+	err = json.Unmarshal(w.Body.Bytes(), &response)
+	if err != nil {
+		t.Fatalf("Failed to parse response body: %v", err)
+	}
+
+    // レスポンスボディの内容の検証
+    if len(response.Data.Cryptos) > 0 {
+        assert.Equal(t, "btc", response.Data.Cryptos[0].Code)
+		assert.Equal(t, 5047113.0, response.Data.Cryptos[0].GetPrice)
+		assert.Equal(t, 0.05, response.Data.Cryptos[0].Quantity)
+		assert.Equal(t, 5956517.0, response.Data.Cryptos[0].CurrentPrice)
+    } else {
+        t.Fatalf("Expected non-empty MarketPrice array")
+    }
+}
+
+
+func TestCreateCryptoE2E(t *testing.T) {
+	db := test.SetupTestDB(t)
+
+	// モックの HTTP レスポンスを設定
+	mockCryptoPrice := `{
+		"success": 1,
+		"data": {
+			"sell": "88.225",
+			"buy": "88.224",
+			"open": "89.720",
+			"high": "90.370",
+			"low": "87.291",
+			"last": "88.242",
+			"vol": "9433775.5445",
+			"timestamp": 1703916974315
+		}
+	}`
+
+	// モックのHTTPクライアント設定
+	mockTransport := &MockHTTPTransport{
+		RoundTripFunc: func(req *http.Request) (*http.Response, error) {
+			responseBody := mockCryptoPrice
+	
+			r := io.NopCloser(bytes.NewReader([]byte(responseBody)))
+			return &http.Response{
+				StatusCode: http.StatusOK,
+				Body:       r,
+			}, nil
+		},
+	}
+
+	mockHTTPClient := &http.Client{Transport: mockTransport}
+	mockMarketPriceRepo := repoMarketPrice.NewCryptoRepository(mockHTTPClient)
+	// オプションを使用してGraphQLサーバーをセットアップ
+	opts := &graphql.SetupOptions{
+		MockHTTPClient: mockHTTPClient,
+		// MarketCryptoRepoにモックリポジトリを指定
+		MarketCryptoRepo: mockMarketPriceRepo,
+	}
+    router := graphql.SetupGraphQLServer(db, opts)
+
+    // テスト用HTTPサーバーのセットアップ
+    ts := httptest.NewServer(router)
+    defer ts.Close()
+
+	// ダミーのアクセストークンを生成
+	token, err := graphql.GenerateTestAccessTokenForUserId(t, 1)
+	if err != nil {
+		t.Fatalf("Failed to generate test access token: %v", err)
+	}
+
+	// GraphQLリクエストの実行
+	query := `mutation {
+		createCrypto(input: {
+		  code: "xrp",
+		  getPrice: 88.0,
+		  quantity: 1
+		}) {
+		  id
+		  code
+		  getPrice
+		  quantity
+		  currentPrice
+		}
+	  }
+	  `
+	  w := graphql.ExecuteGraphQLRequestWithToken(ts.URL, query, token)
+	  t.Log(w.Body)
+
+	// レスポンスボディの解析
+    var response struct {
+        Data struct {
+            CreateCrypto struct {
+                ID string `json:"id"`
+				Code string `json:"code"`
+				GetPrice float64 `json:"getPrice"`
+				Quantity float64 `json:"quantity"`
+                CurrentPrice float64 `json:"currentPrice"`
+            } `json:"createCrypto"`
+        } `json:"data"`
+    }
+
+	err = json.Unmarshal(w.Body.Bytes(), &response)
+	if err != nil {
+		t.Fatalf("Failed to parse response body: %v", err)
+	}
+
+    // レスポンスボディの内容の検証
+	assert.Equal(t, "xrp", response.Data.CreateCrypto.Code)
+	assert.Equal(t, 88.0, response.Data.CreateCrypto.GetPrice)
+	assert.Equal(t, 1.0, response.Data.CreateCrypto.Quantity)
+	assert.Equal(t, 88.242, response.Data.CreateCrypto.CurrentPrice)
+}
